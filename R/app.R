78ea031ac90a2aa2570232c223651061eeb8c913
Daniel Ward <daniel.ward073@gmail.com>
2025-05-16 08:29:40 +0100

    Added initial changes for a simple sensitivity analysis runner

--- R/app.R
+++ R/app.R
@@ -115,3 +115,33 @@ run_dev_dash <- function() {
 
   shinyApp(ui_dev, server_dev)
 }
+
+
+#' Launch the DPM Shiny Dashboard
+#'
+#' @description
+#' This function initializes and launches the interactive DPM (Data Processing & Modeling) Shiny dashboard. It sources the necessary server (`server.R`) and UI (`ui.R`) components, then combines them to create and run the Shiny application.
+#'
+#' @details
+#' The dashboard facilitates data exploration, analysis, and visualization using a variety of tools and techniques. It provides an intuitive interface for users to interact with their data and gain insights.
+#'
+#' Before running this function, ensure that:
+#' - The `server.R` and `ui.R` files are located in a directory named "R" relative to your current working directory.
+#' - All required packages for the dashboard (specified in `server.R` and `ui.R`) are installed.
+#'
+#' @return
+#' This function does not return a value directly. It launches the Shiny app in your default web browser.
+#'
+#' @examples
+#' \dontrun{
+#' # Launch the DPM dashboard (after setting your working directory appropriately)
+#' run_full_dash()
+#' }
+#'
+#' @export
+run_sens_dash <- function() {
+  source(here::here("R/server_sensitivity.R"))
+  source(here::here("R/ui_sensitivity.R"))
+  
+  shinyApp(ui_sens, server_sens)
+}
